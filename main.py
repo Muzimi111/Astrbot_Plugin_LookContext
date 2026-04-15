8646139d532e721b5e90121e01432be87105688c
Muzimi <qw345678nm@163.com>
2026-04-15 13:17:04 +0800

    修改错误钩子

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api.provider import ProviderRequest
 from astrbot.api import logger
@@ -10,10 +10,11 @@ class MyPlugin(Star):
 
     
 
-    @filter.on_llm_response()
+    @filter.on_llm_request()
     async def debug_llm_context(self, event: AstrMessageEvent, req: ProviderRequest):
         """这是一个全局钩子函数，可以在这里观察每次对话传给大模型的提示词（Prompt）和系统提示词（System Prompt）。"""
         logger.info("🔍 [观察插件] 捕获到一次 LLM 请求！")
         logger.info(f"🔵 【历史对话记录 (Contexts)】:\n{req.contexts}")
         logger.info(f"🔴 【当前用户输入 (叠加了旁白)】:\n{req.prompt}")
         logger.info(f"🟢 【系统人设】:\n{req.system_prompt}")
+
